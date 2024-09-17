8ac3d84aad8298105f0ee56f2cc4b301a428b148
Sergii Kliuchnyk <sergiikliuchnyk@gmail.com>
2024-09-17 06:24:08 +0300

    refactoring state with types

--- state/state.go
+++ state/state.go
@@ -18,34 +18,6 @@ type Root struct {
 	Listeners     Listeners
 }
 
-type Ref struct {
-	Uri     Uri
-	Node    *Node
-	Surname string
-	Name    string
-}
-
-type Duplicate struct {
-	Family *Family
-	Member *Member
-	Node   *Node
-	Uri    string
-}
-
-type MarkdownFile struct {
-	Uri      Uri
-	UriParts []string
-}
-
-type (
-	Families   map[string]*Family
-	Members    map[string]*Member
-	NodeRefs   map[Uri]map[*Node]*Member
-	Duplicates map[string][]*Duplicate
-	Refs       []*Ref
-	Listeners  map[string][]func()
-)
-
 func CreateRoot() *Root {
 	return &Root{
 		Families:      make(Families),
---
+++ state/types.go
@@ -0,0 +1,33 @@
+package state
+
+import (
+	. "github.com/redexp/familymarkup-lsp/types"
+)
+
+type Ref struct {
+	Uri     Uri
+	Node    *Node
+	Surname string
+	Name    string
+}
+
+type Duplicate struct {
+	Family *Family
+	Member *Member
+	Node   *Node
+	Uri    string
+}
+
+type MarkdownFile struct {
+	Uri      Uri
+	UriParts []string
+}
+
+type (
+	Families   map[string]*Family
+	Members    map[string]*Member
+	NodeRefs   map[Uri]map[*Node]*Member
+	Duplicates map[string][]*Duplicate
+	Refs       []*Ref
+	Listeners  map[string][]func()
+)
