ca77e8c2fd3dd2331247da02bb5f14ea635a25ae
Sergii Kliuchnyk <sergiikliuchnyk@gmail.com>
2024-09-24 16:46:45 +0300

    added SetState, Remove

--- state/uriset.go
+++ state/uriset.go
@@ -4,14 +4,31 @@ import (
 	. "github.com/redexp/familymarkup-lsp/types"
 )
 
-type UriSet map[Uri]bool
+const (
+	FileCreate = uint8(iota)
+	FileChange
+	FileRename
+	FileDelete
+	FileOpen
+	FileClose
+)
+
+type UriSet map[Uri]uint8
 
 func (uris UriSet) Set(uri Uri) {
-	uris[uri] = true
+	uris[uri] = 0
+}
+
+func (uris UriSet) SetState(uri Uri, val uint8) {
+	uris[uri] = val
 }
 
 func (uris UriSet) Has(uri Uri) bool {
 	_, has := uris[uri]
 
 	return has
+}
+
+func (uris UriSet) Remove(uri Uri) {
+	delete(uris, uri)
 }
