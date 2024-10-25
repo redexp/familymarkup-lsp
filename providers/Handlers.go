718d1023e30975d43d833f9faac9e1f543e812e2
Sergii Kliuchnyk <sergiikliuchnyk@gmail.com>
2024-10-25 16:45:48 +0300

    refactoring

---
+++ providers/Handlers.go
@@ -0,0 +1,53 @@
+package providers
+
+import protocol "github.com/tliron/glsp/protocol_3_16"
+
+func NewProtocolHandlers() *protocol.Handler {
+	return &protocol.Handler{
+		Initialize:                     Initialize,
+		Initialized:                    Initialized,
+		SetTrace:                       SetTrace,
+		CancelRequest:                  CancelRequest,
+		TextDocumentSemanticTokensFull: SemanticTokensFull,
+		TextDocumentDidOpen:            DocOpen,
+		TextDocumentDidChange:          DocChange,
+		TextDocumentDidClose:           DocClose,
+		WorkspaceDidCreateFiles:        DocCreate,
+		WorkspaceDidRenameFiles:        DocRename,
+		WorkspaceDidDeleteFiles:        DocDelete,
+		TextDocumentCompletion:         Completion,
+		TextDocumentDefinition:         Definition,
+		TextDocumentReferences:         References,
+		TextDocumentTypeDefinition:     TypeDefinition,
+		TextDocumentHover:              Hover,
+		TextDocumentDocumentHighlight:  DocumentHighlight,
+		TextDocumentPrepareRename:      PrepareRename,
+		TextDocumentRename:             Rename,
+		TextDocumentFoldingRange:       FoldingRange,
+		TextDocumentCodeAction:         CodeAction,
+		TextDocumentDocumentSymbol:     DocSymbols,
+		CodeActionResolve:              CodeActionResolve,
+	}
+}
+
+func NewWorkspaceHandlers() *WorkspaceHandler {
+	return &WorkspaceHandler{
+		WorkspaceSymbol:        AllSymbols,
+		WorkspaceSymbolResolve: ResolveSymbol,
+	}
+}
+
+func NewTreeHandlers() *TreeHandlers {
+	return &TreeHandlers{
+		TreeFamilies:  TreeFamilies,
+		TreeRelations: TreeRelations,
+		TreeMembers:   TreeMembers,
+		TreeLocation:  TreeLocation,
+	}
+}
+
+func NewConfigurationHandlers() *ConfigurationHandlers {
+	return &ConfigurationHandlers{
+		Change: ConfigurationChange,
+	}
+}
