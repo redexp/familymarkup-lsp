32fc1a342442aa0723ff92937b9beb452912f84f
Sergii Kliuchnyk <sergiikliuchnyk@gmail.com>
2024-10-25 16:45:30 +0300

    added wasm

--- run.sh
+++ run.sh
@@ -6,8 +6,12 @@ case "$1" in
         echo "go build"
         go build -o build/main main.go
         ;;
+    wasm)
+        echo "go build wasm"
+        GOOS=js GOARCH=wasm go build -o build/main.wasm main.go
+        ;;
     *)
         echo "go run"
-        go run .
+        go run main.go
         ;;
 esac
